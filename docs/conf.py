91f581532b7adc236390b29ba7372e549c91ec08
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-06-10 20:49:36 -0700

    docs: integrate doxygen step

--- docs/conf.py
+++ docs/conf.py
@@ -6,34 +6,31 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Biscuit'
-copyright = '2025, Biscuit Authors'
-author = 'Biscuit Authors'
+project = "Biscuit"
+copyright = "2025, Biscuit Authors"
+author = "Biscuit Authors"
 
-version = '0.1'
-release = '0.1'
+version = "0.1"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'breathe',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "breathe",
 ]
 
-breathe_projects = {
-    'Biscuit': 'build/xml'
-}
-breathe_default_project = 'Biscuit'
+breathe_projects = {"Biscuit": "build/xml"}
+breathe_default_project = "Biscuit"
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
